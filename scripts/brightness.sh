e68dc72832c1474c152dcfe8351db9f723cb185b
"luvimsan" <luvimsan@gmail.com>
2025-07-10 17:20:37 +0300

    update notification setup

--- scripts/brightness.sh
+++ scripts/brightness.sh
@@ -2,17 +2,61 @@
 
 STEP="5%"
 STATE_FILE="$HOME/.brightness_level"
+ID_FILE="/tmp/bright_notify_id"
+ICON="🌞"
 
+# Change brightness
 case "$1" in
     up)
-      doas brightnessctl set +$STEP
+        doas brightnessctl set +$STEP
         ;;
     down)
-      doas brightnessctl set $STEP-
+        doas brightnessctl set $STEP-
         ;;
     *)
         echo "Usage: $0 {up|down}"
         exit 1
         ;;
 esac
-brightnessctl get > "$STATE_FILE"
+
+# Save current brightness
+current=$(brightnessctl get)
+echo "$current" > "$STATE_FILE"
+
+# Get percent brightness
+max=$(brightnessctl max)
+percent=$((current * 100 / max))
+
+# Generate smooth bar (20 blocks, Unicode fill levels)
+generate_bar() {
+    local percent=$1
+    local total_blocks=20
+    local full_blocks=$((percent * total_blocks / 100))
+    local partial_index=$(((percent * total_blocks % 100) / 5))
+
+    # Partial fill characters: 0–8 (space to full)
+    local partial_chars=(" " "▏" "▎" "▍" "▌" "▋" "▊" "▉" "█")
+
+    local bar=""
+    for ((i = 1; i <= total_blocks; i++)); do
+        if (( i <= full_blocks )); then
+            bar+="█"
+        elif (( i == full_blocks + 1 && partial_index > 0 )); then
+            bar+=${partial_chars[partial_index]}
+        else
+            bar+="░"
+        fi
+    done
+    echo "$bar"
+}
+
+bar=$(generate_bar "$percent")
+
+# Read last notification ID
+[ -f "$ID_FILE" ] && ID=$(cat "$ID_FILE") || ID=0
+
+# Send minimal, glazy white-styled notification (no extra styling added)
+newid=$(dunstify -r "$ID" -t 1000 "$ICON Brightness" "<b><tt>$bar</tt>  $percent%</b>" -p)
+
+echo "$newid" > "$ID_FILE"
+
--- scripts/volume.sh
+++ scripts/volume.sh
@@ -1,21 +1,71 @@
 #!/bin/sh
 
 arg=$1
+ID_FILE="/tmp/vol_notify_id"
+ICON="🔊"
 
-[ $arg = "up" ] && amixer -q sset Master 5%+
-[ $arg = "down" ] && amixer -q sset Master 5%-
-[ "$arg" = "toggle" ] && {
+case "$arg" in
+  up)
+    amixer -q sset Master 5%+
+    ;;
+  down)
+    amixer -q sset Master 5%-
+    ;;
+  toggle)
     muted=$(amixer get Master | grep '\[off\]')
     if [ -n "$muted" ]; then
         amixer -q set Master unmute
-        amixer -q set Speaker unmute
-        amixer -q set Headphone unmute 2>/dev/null
     else
         amixer -q set Master mute
-        amixer -q set Speaker mute
-        amixer -q set Headphone mute 2>/dev/null
     fi
+    ;;
+  *)
+    echo "Usage: $0 {up|down|toggle}"
+    exit 1
+    ;;
+esac
+
+# Get volume and mute state
+volume=$(amixer get Master | awk -F'[][]' '/Left:/ { print $2 }' | tr -d '%')
+muted=$(amixer get Master | grep '\[off\]')
+
+# Decide icon and volume display
+if [ -n "$muted" ]; then
+    icon="🔇"
+    percent=0
+else
+    icon="$ICON"
+    percent=$volume
+fi
+
+# Generate smooth fancy bar (like brightness)
+generate_bar() {
+    percent=$1
+    total_blocks=20
+    full_blocks=$((percent * total_blocks / 100))
+    partial_index=$(((percent * total_blocks % 100) / 5))
+    chars=(" " "▏" "▎" "▍" "▌" "▋" "▊" "▉" "█")
+
+    bar=""
+    for i in $(seq 1 $total_blocks); do
+        if [ $i -le $full_blocks ]; then
+            bar="${bar}█"
+        elif [ $i -eq $((full_blocks + 1)) ] && [ $partial_index -gt 0 ]; then
+            bar="${bar}${chars[$partial_index]}"
+        else
+            bar="${bar}░"
+        fi
+    done
+    echo "$bar"
 }
 
+bar=$(generate_bar "$percent")
+
+# Show/replace Dunst notification
+[ -f "$ID_FILE" ] && ID=$(cat "$ID_FILE") || ID=0
+newid=$(dunstify -r "$ID" -t 1000 "$icon Volume" "<b><tt>$bar</tt>  $percent%</b>" -p)
+echo "$newid" > "$ID_FILE"
+
+# Signal dwmblocks to update
 pkill -RTMIN+30 dwmblocks
-#$HOME/script/volume_dwm.sh &
+
